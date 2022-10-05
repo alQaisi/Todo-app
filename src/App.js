b0596e5e2b0de961f44c542be7fb512438764c1a
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-10-05 16:59:21 +0300

    main files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router';
+import Container from './components/Container/Container.component';
+import RoutesHandler from './routes/RoutesHandler/RoutesHandler.component';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Container/>}>
+        <Route index element={<RoutesHandler page="all"/>}/>
+        <Route path="active" element={<RoutesHandler page="active"/>}/>
+        <Route path="completed" element={<RoutesHandler page="completed"/>}/>
+      </Route>
+    </Routes>
   );
 }
 
